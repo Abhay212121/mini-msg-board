34912ee733636867c26a44c0a2636f407a0340f5
Abhay <abhay.work.02@gmail.com>
2025-05-22 00:01:47 +0530

    database updated.

--- db.js
+++ db.js
@@ -9,8 +9,8 @@ const message = [
     },
     {
         id: 2,
-        text: 'How are you?!',
-        user: 'Ankit',
+        text: 'Horses are gentle domestic animals that feed on grasses and grains. Sometimes they also feed on hay and straw. They live in a shed called stable. In an essay on a horse, we must mention that they are found all over the world and can be tamed easily for their polite nature.Horses symbolize royalty because of their diligence and gallant beauty. Human beings have domesticated horses back from around 4000BC. In an essay on a horse, we must mention that horses have been used in warfare for their strength and endurance. These fierce warriors can run on all kinds of surfaces. With lean, strong, and long limbs, horses run at extremely high speed because of which they were used as a form of transportation in the ancient era. Even today in some parts of the world, horse-drawn carriages convey royalty.',
+        user: 'Nikhil',
         added: 'Fri, Apr 17th',
         bg: 'bg-[#F4F8D3]',
         rotation: '-rotate-5'
